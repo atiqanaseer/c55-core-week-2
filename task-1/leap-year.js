e91aa32e3fa00ffa559e4000b989a61f70085400
Atiqa Naseer <atiqa.naseer99@gmail.com>
2026-01-21 22:56:31 +0100

    Atiqa week 2 assignment task 1

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -1,9 +1,17 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
+const year = prompt("Enter a year: "); // prompt user to enter a year
+const yearNumber = Number(year); // convert the user input to number
 
-// Write your code here
-// Guidance:
-// Step 1: prompt the user to enter a year
-// Step 2: convert the user input to a number so we can perform calculations
-// Step 3: Implement the logic
+// First check if the number is valid and year is from 1 to 9999
+if (!isNaN(yearNumber) && yearNumber >= 1 && yearNumber <= 9999) {
+    // Then check if the year is leap year or not
+    if (yearNumber % 4 === 0 && (yearNumber % 400 === 0 || yearNumber % 100 !== 0)) {
+        console.log(`Yes, ${yearNumber} is a leap year`);
+    } else {
+        console.log(`No, ${yearNumber} is not a leap year`);
+    }
+} else {
+    console.log("Invalid year!");
+}
