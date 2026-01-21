6f6a3c3e077eddf30efa24d0f385902703ccdcaa
Atiqa Naseer <atiqa.naseer99@gmail.com>
2026-01-21 23:18:03 +0100

    Atiqa week 2 assignment task 2

--- task-2/login.js
+++ task-2/login.js
@@ -4,9 +4,19 @@ import { errorMessage, successMessage } from './app.js';
 let incorrectAttempts = 0;
 
 function onLogin(username, password) {
-  // Write your code here.
-  // Use the variables 'username' and 'password' to access the input values
-  // Use incorrectAttempts to track the number of failed attempts
+  // Check if more than 3 incorrect attempts then show login block error message
+  if (incorrectAttempts > 3) {
+    errorMessage('Login blocked: Too many incorrect attempts');
+    return; // Don't run the code further in this function
+  }
+
+  // Check if any of the two username and password combination are correct then show success else show error
+  if ((username === 'admin' && password === 'Hack1234') || (username === 'user' && password === '7654321')) {
+    successMessage('Logged in successfully');
+  } else {
+    errorMessage('Incorrect credentials');
+    incorrectAttempts++; // Increase incorrect login attempt by 1
+  }
 }
 
 // Do not change the line below
